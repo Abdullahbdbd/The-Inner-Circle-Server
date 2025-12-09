9e14b6fb3e2a67db9ee1802d4090c344323d31c0
Abdullahbdbd <121636151+Abdullahbdbd@users.noreply.github.com>
2025-12-09 18:04:42 +0600

    Delete Lesson In Database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
 
 //middleware
@@ -78,6 +78,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    //delete lesson
+    app.delete('/public-lessons/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query ={_id: new ObjectId(id)}
+        const result = await lessonsCollection.deleteOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
